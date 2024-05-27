7bb7341349198e1f1c6a1c00b025415698e35eba
esaytar <usovaalena01@gmail.com>
2024-05-27 19:03:33 +0400

    /

--- src/blocks/mainPage/NewsBlock.jsx
+++ src/blocks/mainPage/NewsBlock.jsx
@@ -17,7 +17,7 @@ export default function NewsBlock() {
                 throw new Error(`Network response was not ok: ${response.statusText}`);
             }
             let data = await response.json();
-            console.log('Data:', data); 
+            // console.log('Data:', data); 
             return data
         } catch (error) {
             console.error('There was a problem with your fetch operation:', error);
