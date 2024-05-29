43feba7f3a147b68e34b2a880a5d765cb914ce4b
esaytar <usovaalena01@gmail.com>
2024-05-29 23:57:24 +0400

    edited

--- src/blocks/mainPage/NewsBlock.jsx
+++ src/blocks/mainPage/NewsBlock.jsx
@@ -30,19 +30,14 @@ export default function NewsBlock() {
                             link={`https://vk.com/tspk63?w=wall${item.owner_id}_${item.id}`}
                             img={findMaxSizes(item.attachments[0].photo.sizes)}
                         /> 
-                        {console.log(item)}
                     </swiper-slide>
                 ))
                 setNews(newsItems)
-                // console.log('Data:', data)
-                // console.log(data.response.items)
             } catch (error) {
                 console.error('There was a problem with your fetch operation:', error);
             }
         }
-    
         fetchData();
-
     }, [])
 
     function findMaxSizes(array) {
@@ -51,39 +46,9 @@ export default function NewsBlock() {
             if (480 <= item.width <= 600) {
                 url = item.url
             } 
-            console.log(item.width, url)  
         })
         return url
     }
-    
-    const swiperRef = useRef(null);
-
-    useEffect(() => {
-        const swiperContainer = swiperRef.current;
-        const params = {
-            breakpoints: {
-                580: {
-                    slidesPerView: 2,
-                },
-                940: {
-                  slidesPerView: 3,
-                }
-            },
-            injectStyles: [
-            `
-                .swiper {display: flex !important; flex-direction: column; gap: 2rem; overflow: visible !important;}
-                @media (min-width: 1024px) {.swiper {gap: 50px;}}
-                .swiper-pagination {position: relative !important; bottom: 0; top: 0; cursor: inherit !important; display: flex; gap: 10px; align-items: center; justify-content: center;}
-                .swiper-pagination-bullet {width: 10px; height: 10px; margin: 0 !important; background: rgba(23, 99, 244, 0.8) !important;}
-                .swiper-pagination-bullet-active {background: #1763F4 !important;}
-
-            `
-          ]
-        };
-    
-        Object.assign(swiperContainer, params);
-        swiperContainer.initialize();
-    }, []);
 
     const convertToNormalDate = (num) => {
         const date = new Date(num * 1000)
@@ -94,13 +59,36 @@ export default function NewsBlock() {
             hour: '2-digit',
             minute: '2-digit',
         });
-
         return readableDate
     }
 
+    const swiperRef = useRef(null)
+    useEffect(() => {
+        const swiperContainer = swiperRef.current
+        const params = {
+            breakpoints: {
+                580: {
+                    slidesPerView: 2,
+                },
+                940: {
+                    slidesPerView: 3,
+                }
+            },
+            injectStyles: [`
+                .swiper {display: flex !important; flex-direction: column; gap: 2rem; overflow: visible !important;}
+                @media (min-width: 1024px) {.swiper {gap: 50px;}}
+                .swiper-pagination {position: relative !important; bottom: 0; top: 0; cursor: inherit !important; display: flex; gap: 10px; align-items: center; justify-content: center;}
+                .swiper-pagination-bullet {width: 10px; height: 10px; margin: 0 !important; background: rgba(23, 99, 244, 0.8) !important;}
+                .swiper-pagination-bullet-active {background: #1763F4 !important;}
+            `]
+        }
+        Object.assign(swiperContainer, params)
+        swiperContainer.initialize()
+    }, [])
+
     return (
         <div className='flex flex-col lg:gap-10 gap-[1.43rem] w-full'> 
-            <p className='text-grayText text-[1.29rem] lg:text-[1.81rem] font-semibold '>Новости</p>
+            <p className='text-grayText text-[1.29rem] lg:text-[1.81rem] font-semibold'>Новости</p>
             <div className='flex flex-col items-center gap-[3.125rem]'> 
                 <div className='lg:overflow-hidden w-full'>
                     <swiper-container
@@ -111,9 +99,7 @@ export default function NewsBlock() {
                         pagination="true"
                         space-between="10"
                         >
-                            {
-                                news
-                            }
+                            {news}
                     </swiper-container>
                 </div>
             </div>
--- src/components/HeadingInfoPage.jsx
+++ src/components/HeadingInfoPage.jsx
@@ -12,21 +12,20 @@ export default function HeadingInfoPage({listRef, titleList}) {
                 setIsOpened(false)
             }
         }
-        document.addEventListener('click', handleClickOutside);
-        return () => { 
-            document.removeEventListener('click', handleClickOutside);
-        };
+        document.addEventListener('click', handleClickOutside)
+        return () => document.removeEventListener('click', handleClickOutside)
     }, []);
 
     return (
         <div className='w-full flex justify-between items-center flex-col lg:flex-row gap-[1.43rem]'>
             <h1 className='lg:text-[1.81rem] text-grayMainText font-semibold lg:w-10/12 text-[1.28rem]'>Сведения об образовательной организации</h1>
             <div ref={listRef} className='w-full relative text-18'>
-                <div className='flex w-full justify-between border-mainBlue/50 border-[2px] rounded-[40px] px-[1.71rem] py-[0.86rem] lg:px-10 lg:py-5 items-center cursor-pointer' onClick={() => {setIsOpened(!isOpened)}}>
+                <div className='flex w-full justify-between border-mainBlue/50 border-[2px] rounded-[40px] px-[1.71rem] py-[0.86rem] items-center 
+                lg:px-10 lg:py-5 cursor-pointer' onClick={() => {setIsOpened(!isOpened)}}>
                     <p className='text-mainBlue '>{titleList}</p>
                     <ArrowBottom style='stroke-mainBlue w-[23px] h-[12px]'/> 
                 </div>
-                <ul  className={`${isOpened ? 'flex' : 'hidden'} absolute flex-col lg:px-10 lg:py-5 p-[1.43rem] bg-white text-mainBlue gap-5 rounded-[1.43rem] lg:rounded-[2.5rem] z-20 mt-2.5 shadow-newShad`}>
+                <ul className={`${isOpened ? 'flex' : 'hidden'} absolute flex-col lg:px-10 lg:py-5 p-[1.43rem] bg-white text-mainBlue gap-5 rounded-[1.43rem] lg:rounded-[2.5rem] z-[15] mt-2.5 shadow-newShad`}>
                     <li><Link to="/basic-data">Основные сведения</Link></li>
                     <li><Link to="/structure-college">Структура и органы управления образовательной организацией</Link></li>
                     <li><a href="">Документы</a></li>
