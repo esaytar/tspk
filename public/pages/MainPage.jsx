1d5b996b568a24ebbec1cfd92bae5e7b274bcf45
esaytar <usovaalena01@gmail.com>
2024-05-30 08:24:34 +0400

    edited responsive

--- public/pages/MainPage.jsx
+++ public/pages/MainPage.jsx
@@ -12,7 +12,7 @@ export default function MainPage() {
         <title>Главная | Тольяттинский социально-педагогический колледж</title>
       </Helmet>
       <HeadBlock/>
-      <div className='flex flex-col xl:w-[73%] gap-[1.43rem] md:gap-[3.75rem] px-[1.42rem] lg:p-0 w-full'>
+      <div className='flex flex-col xl:w-[73%] gap-[1.43rem] md:gap-[3.75rem] px-[1.42rem] xl:p-0 w-full'>
         <QuickAccessBlock/>
         <NewsBlock/>
         <ContactsBlock/>
