2ef3d5b1a4d532dda2fdf5362fc5d909da53b286
esaytar <usovaalena01@gmail.com>
2024-06-07 21:06:08 +0400

    add table

--- public/pages/college-info/DocumentsPage.jsx
+++ public/pages/college-info/DocumentsPage.jsx
@@ -25,6 +25,72 @@ export default function DocumentsPage({func}) {
                     </InfoString>
                 ))
             }
+            <InfoString 
+                title="Предписание органов осуществляющих контроль (надзор) в сфере образования (до подтверждения органом, осуществляющим государственный контроль (надзор) в сфере образования, исполнения предписания или признания его недействительным в установленном законом порядке) (при наличии)">
+                    {/* <div className='flex gap-1'>
+                        <div>Проверяющая организация</div>
+                        <div>Тема проверки</div>
+                        <div>Дата проведения проверки</div>
+                        <div>Дата проведения проверки</div>
+                        <div>Устранения нарушений</div>
+                    </div>
+                    <div className="grid grid-rows-4 grid-cols-5 justify-items-center gap-1">
+                        <div className='p-10 shadow-newShad bg-white w-full'>ОТДЕЛЕНИЕ ФОНДА ПЕНСИОННОГО И СОЦИАЛЬНОГО СТРАХОВАНИЯ РОССИЙСКОЙ ФЕДЕРАЦИИ ПО САМАРСКОЙ ОБЛАСТИ</div>
+                        <div className='p-10 shadow-newShad bg-white w-full'>Плановая проверка полноты и достоверности предоставляемых страхователем или застрахованным лицом сведений и документов, необходимых для назначения и выплаты страхового обеспечения, а также для возмещения расходов страхователя на выплату социального пособия на погребение</div>
+                        <div className='p-10 shadow-newShad bg-white w-full'>02.03.2023 - 17.03.2023</div>
+                        <div className='p-10 shadow-newShad bg-white w-full'><a href="">Акт выездной проверки</a></div>
+                        <div className='p-10 shadow-newShad bg-white w-full'>Нарушений не выявленно</div>
+                        <div className='p-10 shadow-newShad bg-white w-full'>Прокуратора Комсомольского района г. Тольятти</div>
+                        <div className='p-10 shadow-newShad bg-white w-full'>Защита прав и свобод граждан, охраняемых законом интересов общества и государства Проверка исполнения законодательства об образовании, охране здоровья граждан</div>
+                        <div className='p-10 shadow-newShad bg-white w-full'>15.03.2023 - 20.03.2023</div>
+                        <div className='p-10 shadow-newShad bg-white w-full'><a href="">Решение о проведении проверки</a></div>
+                        <div className='p-10 shadow-newShad bg-white w-full'><a href="">Представление об устранении нарушений</a></div>
+                        <div className='p-10 shadow-newShad bg-white w-full'>Прокуратора Комсомольского района г. Тольятти</div>
+                        <div className='p-10 shadow-newShad bg-white w-full'>Исполнение требований законодательства о противодействии терроризму</div>
+                        <div className='p-10 shadow-newShad bg-white w-full'>29.03.2023 - 29.03.2023</div>
+                        <div className='p-10 shadow-newShad bg-white w-full'><a href="">Требование</a></div>
+                        <div className='p-10 shadow-newShad bg-white w-full'>Нарушений не выявленно</div> 
+                    </div> */}
+
+                    <div className='w-full overflow-hidden'>
+                        <div className='overflow-auto'>
+                            <table className='border-spacing-2 table-fixed text-[1.14rem] lg:text-18'>
+                                <thead>
+                                    <tr className='text-mainBlue w-full'>
+                                        <th className='bg-mainBlue/5 relative px-2.5 py-1 w-[24%]'>Проверяющая организация</th>
+                                        <th className='bg-mainBlue/5 relative px-2.5 py-1 w-[20%]'>Тема проверки</th>
+                                        <th className='bg-mainBlue/5 relative px-2.5 py-1 w-[13%]'>Дата проведения проверки</th>
+                                        <th className='bg-mainBlue/5 relative px-2.5 py-1 w-[21%]'>Итоги проверки, предписания</th>
+                                        <th className='bg-mainBlue/5 relative px-2.5 py-1 w-[21.5%]'>Устранения нарушений</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td className='lg:p-10 p-[1.43rem] bg-white relative text-left align-baseline'>ОТДЕЛЕНИЕ ФОНДА ПЕНСИОННОГО И СОЦИАЛЬНОГО СТРАХОВАНИЯ РОССИЙСКОЙ ФЕДЕРАЦИИ ПО САМАРСКОЙ ОБЛАСТИ</td>
+                                        <td className='lg:p-10 p-[1.43rem] bg-white relative text-left align-baseline'>Плановая проверка полноты и достоверности предоставляемых страхователем или застрахованным лицом сведений и документов, необходимых для назначения и выплаты страхового обеспечения, а также для возмещения расходов страхователя на выплату социального пособия на погребение</td>
+                                        <td className='lg:p-10 p-[1.43rem] bg-white relative text-left align-baseline'>02.03.2023 - 17.03.2023</td>
+                                        <td className='lg:p-10 p-[1.43rem] bg-white relative text-left align-baseline'><a className='underline' href="https://tspk.org/images/oficial_dokument_2023/fond_pensonnogo_i_soc_stahovania_2023.pdf">Акт выездной проверки</a></td>
+                                        <td className='lg:p-10 p-[1.43rem] bg-white relative text-left align-baseline'>Нарушений не выявленно</td>
+                                    </tr>
+                                    <tr>
+                                        <td className='p-10 bg-white relative align-baseline'>Прокуратора Комсомольского района г. Тольятти</td>
+                                        <td className='p-10 bg-white relative align-baseline'>Защита прав и свобод граждан, охраняемых законом интересов общества и государства Проверка исполнения законодательства об образовании, охране здоровья граждан</td>
+                                        <td className='p-10 bg-white relative align-baseline'>15.03.2023 - 20.03.2023</td>
+                                        <td className='p-10 bg-white relative align-baseline'><a className='underline' href="https://tspk.org/images/oficial_dokument_2023/reshenie_o_provedenii_proverki_2023.pdf">Решение о проведении проверки</a></td>
+                                        <td className='p-10 bg-white relative align-baseline'><a className='underline' href="https://tspk.org/images/oficial_dokument_2023/Predstavlenie_TSPK_214-23-213.pdf">Представление об устранении нарушений</a></td>
+                                    </tr>
+                                    <tr> 
+                                        <td className='p-10 bg-white relative align-baseline'>Прокуратора Комсомольского района г. Тольятти</td>
+                                        <td className='p-10 bg-white relative align-baseline'>Исполнение требований законодательства о противодействии терроризму</td>
+                                        <td className='p-10 bg-white relative align-baseline'>29.03.2023 - 29.03.2023</td>
+                                        <td className='p-10 bg-white relative align-baseline'><a className='underline' href="https://tspk.org/images/oficial_dokument_2023/Trebovanie_562-23-213.pdf">Требование</a></td>
+                                        <td className='p-10 bg-white relative align-baseline'>Нарушений не выявленно</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+            </InfoString>
         </div>
     )
 }
