d044fb21c062dc67c0cbd0e71e9983920883f2dd
esaytar <usovaalena01@gmail.com>
2024-06-17 19:39:53 +0400

    build

--- pages/college-info/DocumentsPage.jsx
+++ pages/college-info/DocumentsPage.jsx
@@ -1,4 +1,4 @@
-import React, {useRef, useEffect} from 'react'
+import React, {useEffect} from 'react'
 import {documentsInfo} from '../../../src/data/collegeInfo.jsx'
 import InfoString from '../../../src/components/InfoString.jsx';
 
